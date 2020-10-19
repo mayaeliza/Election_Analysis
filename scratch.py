1c5feb8698acd10af95eb1a88b7af5e96aa6095a
Maya Jefferson <mayajefferson@Mayas-MacBook-Pro.local>
2020-10-18 23:38:15 -0500

    Adding pypoll.py file.

---
+++ scratch.py
@@ -0,0 +1,28 @@
+#Same thing but using "with" instead of open/close
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
+  
+    # Write three counties to the file.
+    txt_file.write("Arapahoe, Denver, Jefferson")
+
+    # Write three counties to the file.
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
+    # Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
